370cf2497253ceed92e58143107402e6498f51e8
krista triviso <krista.triviso91@gmail.com>
2018-06-24 22:51:19 -0400

    herkou build

--- server.js
+++ server.js
@@ -9,12 +9,11 @@ const port         = process.env.PORT || 5000;
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
-app.use(express.static(path.join(__dirname, 'dist')));
-app.use(express.static(path.resolve(__dirname, '../client/build')))
+app.use(express.static(path.resolve(__dirname, './client/build')))
 app.use('/api', router)
 
 app.get('*', function(request, response) {
-  response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+  response.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
